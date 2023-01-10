73dcbe85af351cc85acd2cfb348677991dcc9f65
cameron-schlueter <camschlueter@gmail.com>
2023-01-09 21:33:18 -0500

    getting closer to displaying parsed data. Switched APIs to something more user friendly for me. Reworked a fetch.

--- script.js
+++ script.js
@@ -11,37 +11,22 @@ function updateClock() {
 
   console.log();
 
+fetch('https://api.openweathermap.org/data/3.0/onecall?lat=42.9634&lon=85.6681&exclude=hourly,daily&appid={f0e6ee1a746a465da3532536230801}', {
+  headers: {
+    'Authorization': 'Bearer f0e6ee1a746a465da3532536230801'
+  }
+})
+//this parses the data
+.then(response => response.json())
+.then(data => {
+// why is this reading as typescript?
+    const Temperature: data.current.temp;
+    const Feels Like: data.current.feels_like;
+// stopping point 1/9
+    document.getElementById('displayGrandRapids').innerText = 'Temperature: ${current.temp}';
+    document
 
-  
-
-// fetch, less broken now, trying to call the temp
-
-fetch('https://www.weatherapi.com/my/fields.aspx')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('displayGrandRapids').innerHTML = 
-    '<p>Temperature: ${temp_f}</p>';
   })
   .catch(error => {
     console.error(error);
   });
-
-// 
-
-//what does this do
-const https = require('https://www.weatherapi.com/my/fields.aspx');
-
-const options = {
-  hostname: 'https://www.weatherapi.com/',
-  path: '/endpoint',
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer f0e6ee1a746a465da3532536230801',
-    // location for weather
-    body: JSON.stringify({
-    q: 'Grand Rapids,US',
-    appid: 'f0e6ee1a746a465da3532536230801'
-    })
-  }
-}
